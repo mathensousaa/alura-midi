cb48a6f740c442a1891e9d9889448ba179297721
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-03-07 18:13:04 -0300

    Anonymous functions in Javascript

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('Olá mundo!');
+function playSound (audioId) {
+    document.querySelector(audioId).play();
+}
+
+const keysList = document.querySelectorAll('.tecla');
+
+let count = 0;
+
+while (count < keysList.length) {
+    keysList[count].onclick = function () {
+        playSound('#som_tecla_pom');
+    };
+    
+    count++;
+}
