316d7287ea79d5517ff2f56ad3b4736c945b2f6c
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-04-22 17:10:06 -0300

    Finalizando curso

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-function playSound (audioId) {
-    document.querySelector(audioId).play();
+playSound = (audioSelector) => {
+    const element = document.querySelector(audioSelector);
+
+    element != null && element.localName === 'audio' ? element.play() : console.log('Elemento não encontrado');
 }
 
 const keysList = document.querySelectorAll('.tecla');
 
-let count = 0;
+for (let count = 0; count < keysList.length; count++) {
+    const key = keysList[count];
+    const instrument = key.classList[1];
+    const idAudio = `#som_${instrument}`;
 
-while (count < keysList.length) {
-    keysList[count].onclick = function () {
-        playSound('#som_tecla_pom');
-    };
-    
-    count++;
+    key.onclick = () => playSound(idAudio);
+
+    key.onkeydown = (event) => event.code === 'Enter' || event.code === 'Space' ? key.classList.add('ativa') : '';
+    key.onkeyup = () => key.classList.remove('ativa');
 }
